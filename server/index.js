2970474effd62c7bac5f1b0a0cd027a19171be19
ayushkumar3820 <kumarayush382000@gamil.com>
2025-08-17 21:06:08 +0530

    dsd

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-import express from 'express';
-import cors from 'cors';
-import http from 'http';
-import { Server } from 'socket.io';
-import connectDB from './config/database.js';
-import authRoutes from './routes/auth.js';
-import projectRoutes from './routes/project.js';
-import taskRoutes from './routes/task.js';
-import { configDotenv } from 'dotenv';
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+import connectDB from "./config/database.js";
+import authRoutes from "./routes/auth.js";
+import projectRoutes from "./routes/project.js";
+import taskRoutes from "./routes/task.js";
+import { configDotenv } from "dotenv";
 configDotenv();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: 'https://mern-task-brainy-dx-company-psx2.vercel.app/',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    },
+  cors: {
+    origin: "https://mern-task-brainy-dx-company-psx2.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
 });
 
 app.use(cors());
@@ -23,18 +23,21 @@ app.use(express.json());
 
 connectDB();
 
-app.use('/api/auth', authRoutes);
-app.use('/api/projects', projectRoutes);
-app.use('/api/tasks', taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
 
-io.on('connection', (socket) => {
-    console.log('New client connected');
-    socket.on('taskUpdate', (task) => {
-        io.emit('taskUpdated', task);
-    });
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-    });
+app.get("/", (req, res) => {
+  res.send("task are run in server ");
+});
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  socket.on("taskUpdate", (task) => {
+    io.emit("taskUpdated", task);
+  });
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
 const PORT = process.env.PORT || 5000;
