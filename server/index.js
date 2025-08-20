798d32aa1622630b4ef43373b068731996005236
Ayush4290 <ayush@weboum.com>
2025-08-20 13:17:58 +0530

    update code date 0/08/2025

--- server/index.js
+++ server/index.js
@@ -23,7 +23,14 @@ const io = new Server(server, {
 });
 
 // ✅ Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://mern-task-brainy-dx-company-1nvy.vercel.app",
+    "http://localhost:3000"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(express.json()); // important for JSON body parsing
 
 // ✅ Connect Database
